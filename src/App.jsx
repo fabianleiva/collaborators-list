d4d94c2699fd0e7a4cabef340b7e6840cff19c84
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2023-11-11 17:38:20 -0300

    add useState, search component and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useState } from "react";
 import { collaboratorsBase } from "./assets/js/collaboratorsBase";
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Background from "./components/Background"
 import Form from "./components/Form";
 import List from "./components/List";
-import Seeker from "./components/Seeker";
-import Busqueda from "./components/Busqueda";
+import Search from "./components/Search";
 
 function App() {
   const [data, setData] = useState(collaboratorsBase);
+  const [dataFiltered, setDataFiltered] = useState(collaboratorsBase);
   const [error, setError] = useState("");
 
   return (
     <>
       <div className="container">
-        <Busqueda datos={datos} dataFiltro={setDataFiltro} />
-        <section className="list">
-          <h4>Lista de colaboradores</h4>
+        <section className="listSection">
+          <h4>- Lista de colaboradores -</h4>
+          <Search
+            dataFiltered={dataFiltered}
+            setDataFiltered={setDataFiltered}
+          />
           <List data={data} />
         </section>
-        <section className="form">
-          <h4>Agregar colaborador</h4>
+        <section className="formSection">
+          <h4>Agregar colaborador:</h4>
           <Form setError={setError} data={data} setData={setData} />
           <Alert error={error} />
         </section>
       </div>
+      <Background />
     </>
   );
 }
