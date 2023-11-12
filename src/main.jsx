7aa2ed95faf971e717c6877da5269dee18e94d68
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2023-11-11 23:16:14 -0300

    add background

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Background from "./components/Background";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Background />
+  </React.StrictMode>
+);
